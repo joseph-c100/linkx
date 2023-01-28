45cdf8d1553a6153f8af1bc562773103a68ec562
Joseph C <111119464+joseph-c100@users.noreply.github.com>
2023-01-28 12:52:18 +0000

    updated supabase code

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-// import the supabase library and establish the connection with the database
+// establish the connection with the database and create client
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+const supabase = createClient('https://vmafcgiotxmfbwcqrifn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtYWZjZ2lvdHhtZmJ3Y3FyaWZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ4OTk4OTcsImV4cCI6MTk5MDQ3NTg5N30.aUV6EOA449unU2Nb8mprEu0_yHafI9_MeZYVL_F-sUw')
 
-const { createClient } = supabase
-const _supabase = createClient('https://vmafcgiotxmfbwcqrifn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtYWZjZ2lvdHhtZmJ3Y3FyaWZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ4OTk4OTcsImV4cCI6MTk5MDQ3NTg5N30.aUV6EOA449unU2Nb8mprEu0_yHafI9_MeZYVL_F-sUw')
+// Insert a row
+const { data, error } = await supabase
+    .from('linkks_local')
+    .insert([
+        { list_title: 'test name' },
+    ]);
 
-console.log('Supabase Instance: ', _supabase)
-
+// Did it work?
+console.log(data, error);
 
 // google sign in and sign out - function needs to be called when button is clicked.
 async function signInWithGoogle() {
