e2fc6d4cad640baa8ec20f3954681ee0ec7a40c8
Joseph C <111119464+joseph-c100@users.noreply.github.com>
2023-02-03 15:11:28 +0000

    getting confused now, added an export of currentUserEmail from login.js which should work

--- app.js
+++ app.js
@@ -2,28 +2,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 const supabase = createClient('https://vmafcgiotxmfbwcqrifn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtYWZjZ2lvdHhtZmJ3Y3FyaWZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ4OTk4OTcsImV4cCI6MTk5MDQ3NTg5N30.aUV6EOA449unU2Nb8mprEu0_yHafI9_MeZYVL_F-sUw')
 // ---------------------------------------------
+import { currentUserEmail } from './login';
 
-// Supabase Magic Link sign in and sign out
-let currentUserEmail;
-
-var pathname = window.location.pathname;
-if (pathname = 'index.html') {
-    document.getElementById('loginBtn').addEventListener("click", async function signInWithEmail() {
-        currentUserEmail = prompt("Enter your email to receive the Magic Link:");
-        const { data, error } = await supabase.auth.signInWithOtp({
-        email: currentUserEmail,
-        options: {
-            emailRedirectTo: 'https://linkks.netlify.app/app.html',
-        },
-        })
-    })
-}
-
-// document.getElementById('logoutBtn').addEventListener("click", async function signout() {
-//     const { error } = await supabase.auth.signOut();
-// });
-
-// ----------------------------------
 
 // adds textbox value i.e. the link to link section when user hits enter
 document.querySelector("#textbox").addEventListener("keyup", event => {
